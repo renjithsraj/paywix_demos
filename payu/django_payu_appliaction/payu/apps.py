4e9f390fb43e217f6d0dff315e0cba8a009b2132
renjith s raj <renjithsraj@live.com>
2019-09-16 09:20:54 +0530

    update data

--- payu/django_payu_appliaction/payu/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PayuConfig(AppConfig):
-    name = 'payu'
--- payu/django_payu_appliaction/payu/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "appname"
-urlpatterns = [
-    path('payment', views.payment, name="payment"),
-    path('payment/success', views.payment_success, name="payment_success"),
-    path('payment/failure', views.payment_failure, name="payment_failure"),
-]
