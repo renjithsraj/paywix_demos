4966638dbfd5931bb9284bbb2c150943c72c6cc2
Renjith S Raj <renjith@FS-40.local>
2020-07-11 12:48:55 +0530

    clear branch

--- paywix_demo/paywixtest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaywixtestConfig(AppConfig):
-    name = 'paywixtest'
--- paywix_demo/paywixtest/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def paywix_stripe_manage(request):
-    print (request.POST)
-    import pdb; pdb.set_trace();
